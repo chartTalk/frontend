0599c75f156fa9faa8f2e8fa68daf2a6665920aa
ohsunho <dhwkdrns99@gmail.com>
2025-07-04 20:44:10 +0900

    feat: 메인 페이지 UI 구현

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ChatPage from "./pages/ChatPage";
+
+function App() {
+  return (
+      <ChatPage />
+  );
+}
+
+export default App;
---
+++ src/components/ChatHeader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChatHeader = () => {
+  return (
+    <header className="h-14 px-6 flex items-center border-b border-gray-300 bg-white shadow-sm">
+      <h1 className="text-lg font-semibold text-gray-800">ChartTalk</h1>
+    </header>
+  );
+};
+
+export default ChatHeader;
